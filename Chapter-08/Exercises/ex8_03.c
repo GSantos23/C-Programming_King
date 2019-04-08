d45fbbc6a80c6d19e1933cf7d160b5401cbed104
GSantos23 <santosgerson64@gmail.com>
2019-04-08 12:30:04 -0600

    Continuous repdigit

---
+++ Chapter-08/Exercises/ex8_03.c
@@ -0,0 +1,43 @@
+/* Modify  the repdigit.c program of Section 8.1 so that the user can enter more
+than one number to be tested for repeated digits. The program should terminate 
+when the user enters a number that's less than equal to 0*/
+
+// Checks numbers for repeated digits
+
+#include <stdbool.h>
+#include <stdio.h>
+
+int main (void)
+{
+    bool digit_seen[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    do
+    {
+        while (n > 0)
+        {
+            digit = n % 10;
+            if (digit_seen [digit])
+                break;
+            digit_seen[digit] = true;
+            n /= 10;
+        }
+
+        if (n > 0)
+            printf("Repeated digit\n");
+        else
+            printf("No repeated digit\n");
+
+        printf("Enter a number: ");
+        scanf("%ld", &n);
+        
+    } while (n > 0);
+
+
+
+    return 0;
+}
