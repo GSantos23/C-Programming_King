32faef347f4fc964fa9b7e3cfe5a98ecdd9336b9
GSantos23 <santosgerson64@gmail.com>
2019-09-16 22:15:27 -0600

    hello wold

---
+++ Chapter-02/Exercises/ex2_01.c
@@ -0,0 +1,21 @@
+/*
+Create and run Kernighan and Ritchie's famous "hello, world" program.
+
+Do you get a warning message from the compiler? If so, what's needed to make
+it go away?
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+/*
+I did not get an error message.
+Compiler version:
+
+gcc (Gentoo 8.3.0-r1 p1.1) 8.3.0
+
+*/
