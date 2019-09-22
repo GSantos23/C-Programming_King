5379564a8aa41f165951d1bfefce56a06d126f53
GSantos23 <santosgerson64@gmail.com>
2019-09-21 18:33:45 -0600

    Add exercise 2.3

---
+++ Chapter-02/Exercises/ex2_03.c
@@ -0,0 +1,21 @@
+/*
+Condense the dweight.c program by (1) replacing assigments to height, length, 
+and width with initializers and (2) removing the weight variable, instead
+calculating (volume + 165)/166 within the last printf.
+*/
+
+/* Computes the dimensional weight of a 12" x 10" x 8" box */
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 8, length = 12, width = 10;
+
+    int volume = height * length * width;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Volume(cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
+
+    return 0;
+}
