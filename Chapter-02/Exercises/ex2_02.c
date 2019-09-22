d5c851378e3394929de2a83e93241de35e10ed3b
GSantos23 <santosgerson64@gmail.com>
2019-09-21 18:30:01 -0600

    Add exercise 2.2

---
+++ Chapter-02/Exercises/ex2_02.c
@@ -0,0 +1,39 @@
+/*
+Consider the following program ...
+
+    a) Identify the directives and statements in this program.
+    b) What output does the program produce?
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinsons's Law: \nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
+
+
+
+/*
+Answers:
+
+(a)
+
+Directives -- Directives always start with #
+
+#include <stdio.h> 
+
+
+Statements -- A command to be executed
+
+
+(b)
+
+Parkinsons's Law: 
+Work expands so as to fill the time
+available for its completion.
+
+*/
