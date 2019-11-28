7ad7ef5540c3a6ce78fbd131190c23031ca6bf72
GSantos23 <santosgerson64@gmail.com>
2019-11-27 20:56:30 -0700

    Add exercise 6.2

---
+++ Chapter-06/Exercises/ex6_02.c
@@ -0,0 +1,29 @@
+/*
+What output does the following program fragment produce?
+
+i = 9384;
+do {
+    printf("%d ", i);
+    i /= 10;
+} while (i > 0);
+
+*/
+
+/*
+ * Answer: 9384 938 93 9
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 9384;
+    do {
+        printf("%d ", i);
+        i /=10;
+    }while (i > 0);
+
+    printf("\n");
+
+    return 0;
+}
