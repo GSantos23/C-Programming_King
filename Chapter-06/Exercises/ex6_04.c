d8cc831f2228fbffa0b5857e0a526abf60c0fba5
GSantos23 <santosgerson64@gmail.com>
2019-11-27 21:07:30 -0700

    Add exercise 6.4

---
+++ Chapter-06/Exercises/ex6_04.c
@@ -0,0 +1,11 @@
+/**
+ * Which one of the following statements is not equivalent to the other two (assuming
+ *  that the loop bodies are the same)?
+ *
+ *  (a) for (i = 0; i < 0; i++) ...
+ *  (b) for (i = 0; i < 0; ++i) ...
+ *  (c) for (i = 0; i++ < 0; ) ...
+ *
+ *  Option  (c) -- because increments before comparing the condition.
+ *  
+ */
