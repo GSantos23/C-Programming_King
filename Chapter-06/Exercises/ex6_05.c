90fdec93d45bf183b9564c4ab589bc483833f455
GSantos23 <santosgerson64@gmail.com>
2019-11-28 20:43:32 -0700

    Add exercise 6.5

---
+++ Chapter-06/Exercises/ex6_05.c
@@ -0,0 +1,11 @@
+/**
+ * Which one of the following statements is not equivalent to the other two (assuming
+ *  that the loop bodies are the same)?
+ *
+ *  (a) while (i < 0) {...}
+ *  (b) for (; i < 0;) {...}
+ *  (c) do {...} while (i < 10 );
+ *
+ */
+
+// Option (c) given that test the condition one more time than the other two
