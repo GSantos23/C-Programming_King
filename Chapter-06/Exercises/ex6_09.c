2208525fb47abf80d6a0dfef46ebaf224fa4ffa2
GSantos23 <santosgerson64@gmail.com>
2019-11-30 22:14:42 -0700

    Add exercise 6.9

---
+++ Chapter-06/Exercises/ex6_09.c
@@ -0,0 +1,21 @@
+/**  
+ * Translate the for statement of Exercise 8 into an equivalent while statement.
+ * You will need one statement in addition to the while loop itself.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 10;
+
+	while(i >=1)
+	{
+		printf("%d ", i++);
+		i /= 2;
+	}
+
+    printf("\n");
+
+    return 0;
+}
