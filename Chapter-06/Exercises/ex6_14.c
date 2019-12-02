95bc9d462758214b392bc1ed3c7b5965c5b13331
GSantos23 <santosgerson64@gmail.com>
2019-12-01 21:37:58 -0700

    Add exercise 6.14

---
+++ Chapter-06/Exercises/ex6_14.c
@@ -0,0 +1,10 @@
+/*
+Find the error in the following program fragment and fix it.
+
+if (n %  2 == 0);
+    printf("n is even\n");
+
+*/
+
+
+// The error is the semicolon at the end of the if statement.
