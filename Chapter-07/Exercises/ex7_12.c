adaef4802713d87f9778d602b867d2a651ff6669
GSantos23 <santosgerson64@gmail.com>
2019-12-04 13:16:42 -0700

    Add type conversion - exercise 7.12

---
+++ Chapter-07/Exercises/ex7_12.c
@@ -0,0 +1,7 @@
+/* Suppose that i is a variable of type int, f is a variable of type float, and
+ * d is a variable of type double. Explain what conversions take place during
+ * the execution of the following statement:
+ * d = i + f; */
+
+// d = i + f;
+// double = int + float; -> double
