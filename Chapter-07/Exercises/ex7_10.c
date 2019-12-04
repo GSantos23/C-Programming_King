7531c0d8f88920344aaa54725bb494af63158912
GSantos23 <santosgerson64@gmail.com>
2019-12-04 13:11:42 -0700

    Add type conversion - exercise 7.10

---
+++ Chapter-07/Exercises/ex7_10.c
@@ -0,0 +1,5 @@
+/* Suppose that i is a variable of type int, j is a variable of type long, and
+ * k is a variable of type unsigned int. What is the type of expression
+ * i + (int) j * k? */
+
+// int + (int) long * unsigned int == unsigned int
