c597a793974cda23d63ba4649e04f5c27ca5f969
GSantos23 <santosgerson64@gmail.com>
2019-12-06 21:39:35 -0700

    [exercise 8.7] -- Add C99 designated initializer for seven-segment display

--- Chapter-08/Exercises/ex8_07.c
+++ Chapter-08/Exercises/ex8_07.c
@@ -1,62 +1,25 @@
-/* Write a program that read a 5 x 5 array of integers and then prints the rows
-sums and the columns sums:
-
-Enter row 1: 8 3  9 0 10
-Enter row 2: 3 5 17 1  1
-Enter row 3: 2 8 6 23 1
-Enter row 4: 15 7 3 2 9
-Enter row 5: 6 14 2 6 0
-
-Row totals: 30 27 40 36 28
-Column totals: 34 37 37 32 31
+/*
+ * Using the shortcuts described in Section 8.2, shrink the initializer for the
+ * segments array (Exercise 6) as much as you can.
 */
 
 #include <stdio.h>
 
 int main(void)
-{
-    int valArr[5][5];
-    int rowSum[5], colSum[5];
-    int sum = 0 , sum2 = 0;;
-    int value = 0, value2 = 0;
+{								//0  1  2  3  4  5  6
+	const int segments[10][7] = {{1, 1, 1, 1, 1, 1}, // 0
+								 {0, 1, 1}, // 1
+								 {1, 1, 0, 1, 1, 0, 1}, // 2
+								 {1, 1, 1, 1, 0, 0, 1}, // 3
+								 {0, 1, 1, 0, 0, 1, 1}, // 4
+								 {1, 0, 1, 1, 0, 1, 1}, // 5
+								 {1, 0, 1, 1, 1, 1, 1}, // 6			_
+								 {1, 1, 1, 0, 0, 0, 1}, // 7, assuming  -|
+								 {1, 1, 1, 1, 1, 1, 1}, // 8
+								 {1, 1, 1, 1, 0, 1, 1}, // 9
+								};
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Enter row %d: ", i+1);
-        for (int j = 0; j < 5; j++)
-        {
-            scanf("%d", &value);
-            valArr[i][j] = value;
-            sum += value;
-        }
+	printf("\n");
 
-        rowSum[i] = sum;
-        sum = 0;
-    }
-
-    printf("Row totals: "); 
-    for (int i = 0; i < 5; i++)
-        printf("%d ", rowSum[i]);
-
-    printf("\n");
-    printf("Column totals: ");
-
-    for (int j = 0; j < 5; j++)
-    {
-        for (int k = 0; k < 5; k++)
-        {
-            value2 = valArr[k][j];
-            sum2 += value2;
-        }
-
-        colSum[j] = sum2;
-        sum2 = 0;
-    }
-
-    for (int i = 0; i < 5; i++)
-        printf("%d ", colSum[i]);
-
-    printf("\n");
-
-    return 0;
+	return 0;
 }
