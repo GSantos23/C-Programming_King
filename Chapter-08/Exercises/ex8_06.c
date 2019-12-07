ad5e18ca3f6b2e780f660c04ae0f4963c42ecac5
GSantos23 <santosgerson64@gmail.com>
2019-12-06 21:33:59 -0700

    [exercise 8.6] -- Add initializers for seven-segment display

--- Chapter-08/Exercises/ex8_06.c
+++ Chapter-08/Exercises/ex8_06.c
@@ -1,62 +1,42 @@
-/*The prototypical Internet newbie is a fellow named B1FF, who has a unique way
-of wirting messages. Here's a typical B1FF communique:
-
-H3Y DUD3, C 15 R1LLY COOL!!!!!!!!!!
-
-Write a "B1FF filter" that reads a message entered by the user and translates it
-into B1FF speak:
-
-Enter message: Hey dude, C is rilly cool
-In B1FF-speak: H3Y DUD3, C is R1lly COOL!!!!!!!!
-
-Your program should convert the message to upper-case letter, substitute digits
-for certin letters (A->4, B->8, E -> 3, I-> 1, O->0, S->5), and then append 10 
-or so exclamation marks. Hint: Store the originial message in a array of 
-characters, then go back through the array, translating and printing characters
-one by one.
+/*
+ * Calculators, watches, and other electronic devices often rely on seven-segment
+ * displays for numerical output. To form a digit, such devices "turn on" some of
+ * the seven segments while leaving other "off":
+ *   _      _   _       _   _  _   _   _
+ *  | |  |  _|  _| |_| |_  |_   | |_| |_|
+ *  |_|  | |_   _|   |  _| |_|  | |_|  _|
+ *
+ *  Suppose that we want to set up an array that remembers which segments should
+ *  be "on"  for each digit. Let's number the segments as follows:
+ *
+ *   ___0
+ * 5|_6_|1
+ * 4|___|2
+ *   3
+ *
+ * Here's what the array might look like, with each row representing one digit:
+ *  const int segments[10][7] = {{1, 1, 1, 1, 1 , 1, 0},...};
+ *
+ *  I've give you the first row of the initializer; fill in the rest
 */
 
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 
 int main(void)
-{
-    char msg[30];
+{								//0  1  2  3  4  5  6
+	const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, // 0
+								 {0, 1, 1, 0, 0, 0, 0}, // 1
+								 {1, 1, 0, 1, 1, 0, 1}, // 2
+								 {1, 1, 1, 1, 0, 0, 1}, // 3
+								 {0, 1, 1, 0, 0, 1, 1}, // 4
+								 {1, 0, 1, 1, 0, 1, 1}, // 5
+								 {1, 0, 1, 1, 1, 1, 1}, // 6
+								 {1, 1, 1, 0, 0, 0, 1}, // 7
+								 {1, 1, 1, 1, 1, 1, 1}, // 8
+								 {1, 1, 1, 1, 0, 1, 1}, // 9
+								};
 
-    printf("Enter message: ");
-    for(int i = 0; i < 30; i++)
-    {
-        scanf("%c", &msg[i]);
+	printf("\n");
 
-        msg[i] = toupper(msg[i]);
-
-        if (msg[i] == '\n')
-            break;
-    }
-
-    printf("In B1FF-speak: ");
-    for (int i = 0; i < 35; i++)
-    {
-        if (msg[i] == 'A')
-            msg[i] = '4';
-        else if (msg[i] == 'B')
-            msg[i] = '8';
-        else if (msg[i] == 'E')
-            msg[i] = '3';
-        else if (msg[i] == 'I')
-            msg[i] = '1';
-        else if (msg[i] == 'O')
-            msg[i] = '0';
-        else if (msg[i] == 'S')
-            msg[i] = '5';
-        else if (i >= 25)
-            msg[i] = '!';
-
-       printf("%c", msg[i]);
-    }
-
-    printf("\n");
-
-    return 0;
+	return 0;
 }
