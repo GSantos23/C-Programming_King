c004525e3ba01574b3c265010561a494b2d86bc5
GSantos23 <santosgerson64@gmail.com>
2019-12-08 20:53:34 -0700

    [exercise 8.11] -- Declare and display checker_board

--- Chapter-08/Exercises/ex8_11.c
+++ Chapter-08/Exercises/ex8_11.c
@@ -1,70 +1,50 @@
 /*
-Modify Programming Project 4 from Chapter 7 so that the program labels its output:
+ * Write a program fragment that declares an 8 X 8 char array named checker_board
+ * and then uses a loop to store the following data into the array (one character
+ * per array element):
 
-Enter phone number: 1-800-COL-LECT
-In numeric form: 1-800-265-5328
+    B R B R B R B R
+    R B R B R B R B
+    B R B R B R B R
+    R B R B R B R B
+    B R B R B R B R
+    R B R B R B R B
+    B R B R B R B R
+    R B R B R B R B
 
-The program will need to store the phone number (either in its original form or
-in its numeric form) in an array of characters until it can be printed. You may 
-assume that the phone number is no more than 15 characters long.
-*/  
+ * HINT: The element row i, column j, should be the letter B if i + j is an even
+ * number.
+*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    char ch;
-    char phoneNumber[15] = {'0'};
-    int counter = 0;
+	char checker_board[8][8] = {0};
+	char pieces_black = 'B';
+	char pieces_red = 'R';
 
-    printf("Enter phone number: ");
-    ch = getchar();
-    while(ch != '\n')
-    {
-        
-        switch (ch)
-        {
-            case 'A': case 'B': case 'C': 
-                phoneNumber[counter] = '2';
-                break;
-            case 'D': case 'E': case 'F': 
-                phoneNumber[counter] = '3';
-                break;
-            case 'G': case 'H': case 'I': 
-                phoneNumber[counter] = '4';
-                break;
-            case 'J': case 'K': case 'L': 
-                phoneNumber[counter] = '5';
-                break;
-            case 'M': case 'N': case 'O':   
-                phoneNumber[counter] = '6';
-                break;
-            case 'P': case 'R': case 'S':    
-                phoneNumber[counter] = '7';
-                break;
-            case 'T': case 'U': case 'V':     
-                phoneNumber[counter] = '8';
-                break;
-            case 'W': case 'X': case 'Y': 
-                phoneNumber[counter] = '9';
-                break;                
-            default:    
-                phoneNumber[counter] = ch;
-                break;
-        }
+	printf("Display checker_board:\n\n");
 
-        counter++;
-        ch = getchar();
-    }
+	for (int row = 0; row < 8; row++)
+	{
+		for (int col = 0; col < 8; col++)
+		{
+			if ((row + col) % 2 == 0)
+			{
+				checker_board[row][col] = pieces_black;
+				printf("%c ", checker_board[row][col]);
+			}
+			else
+			{
+				checker_board[row][col] = pieces_red;
+				printf("%c ", checker_board[row][col]);				
+			}
 
-    printf("In numeric form: ");
-    
-    for (int i = 0; i < counter; i++)
-    {
-        printf("%c", phoneNumber[i]);
-    }
+		}
+		printf("\n");
+			}
 
-    printf("\n");
+	return 0;
 
-    return 0;
 }
