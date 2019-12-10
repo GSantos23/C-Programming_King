69a193b2f7cc66eb0181289f875e8bc529bde5d7
GSantos23 <santosgerson64@gmail.com>
2019-12-09 18:48:40 -0700

    [exercise 10.3] -- Number of variables i

---
+++ Chapter-10/Exercises/ex3.c
@@ -0,0 +1,9 @@
+/*
+ * Suppose that a program has only one function (main). How many different
+ * variables named i could this program contain?
+ */
+
+/*
+A global variable i, a set of local variable i depending of the number of scope,
+also depends of the system memmory.
+ */
